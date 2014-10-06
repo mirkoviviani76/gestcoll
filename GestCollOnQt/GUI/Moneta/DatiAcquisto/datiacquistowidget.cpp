cb22177fcd5eacd9973d1184039fdb880cf3da51
mirkoviviani76@gmail.com <mirkoviviani76@gmail.com@39bd2d87-4ba9-2ad7-ec91-730cd3cc0526>
2014-10-06 12:55:51 +0000

    Dati di acquisto sono ora un widget e non piu' una tabella

--- GestCollOnQt/GUI/Moneta/DatiAcquisto/datiacquistowidget.cpp
+++ GestCollOnQt/GUI/Moneta/DatiAcquisto/datiacquistowidget.cpp
@@ -6,10 +6,6 @@ DatiAcquistoWidget::DatiAcquistoWidget(QWidget *parent) :
     ui(new Ui::DatiAcquistoWidget)
 {
     ui->setupUi(this);
-    this->modelloDatiAcquisto = new DatiAcquistoModel(this);
-    this->ui->datiAcquistoView->setModel(this->modelloDatiAcquisto);
-    connect(this->modelloDatiAcquisto, SIGNAL(dataChanged(QModelIndex,QModelIndex)), this, SIGNAL(changesOccurred()));
-    this->ui->datiAcquistoView->setItemDelegate(new DatiAcquistoDelegate(this));
 }
 
 DatiAcquistoWidget::~DatiAcquistoWidget()
@@ -19,22 +15,53 @@ DatiAcquistoWidget::~DatiAcquistoWidget()
 
 void DatiAcquistoWidget::setData(gestColl::coins::datiAcquisto* datiAcquisto)
 {
-    this->modelloDatiAcquisto->clear();
     this->datiAcquisto = datiAcquisto;
-    this->modelloDatiAcquisto->appendRow(datiAcquisto);
 
-    this->ui->datiAcquistoView->resizeColumnsToContents();
+    this->ui->luogo->setText(QString::fromStdWString(this->datiAcquisto->luogo()));
+    QDate dataCorrente;
+    dataCorrente.setDate(this->datiAcquisto->data().year(), this->datiAcquisto->data().month(), this->datiAcquisto->data().day());
+    this->ui->data->setDate(dataCorrente);
+    this->ui->prezzo->setValue(this->datiAcquisto->prezzo().valore());
+    this->ui->prezzo->setSuffix(" "+QString::fromStdWString(this->datiAcquisto->prezzo().unita()));
+
 }
 
 void DatiAcquistoWidget::setEditable(bool editable)
 {
     if (editable) {
-        this->ui->datiAcquistoView->setEditTriggers(QAbstractItemView::DoubleClicked);
-        this->ui->datiAcquistoView->setSelectionBehavior(QAbstractItemView::SelectItems);
-        this->ui->datiAcquistoView->setSelectionMode(QAbstractItemView::SingleSelection);
+        this->ui->prezzo->setButtonSymbols(QAbstractSpinBox::UpDownArrows);
     } else {
-        this->ui->datiAcquistoView->setEditTriggers(QAbstractItemView::NoEditTriggers);
-        this->ui->datiAcquistoView->setSelectionBehavior(QAbstractItemView::SelectRows);
-        this->ui->datiAcquistoView->setSelectionMode(QAbstractItemView::SingleSelection);
+        this->ui->prezzo->setButtonSymbols(QAbstractSpinBox::NoButtons);
     }
+
+    this->ui->data->setCalendarPopup(true);
+
+    this->ui->luogo->setReadOnly(!editable);
+    this->ui->data->setReadOnly(!editable);
+    this->ui->prezzo->setReadOnly(!editable);
+
+    this->ui->luogo->setFrame(editable);
+    this->ui->data->setFrame(editable);
+    this->ui->prezzo->setFrame(editable);
+
+}
+
+void DatiAcquistoWidget::on_luogo_editingFinished()
+{
+    this->datiAcquisto->luogo(this->ui->luogo->text().toStdWString());
+    emit changesOccurred();
+}
+
+void DatiAcquistoWidget::on_data_editingFinished()
+{
+    QDate date = this->ui->data->date();
+    xml_schema::date xmlData(date.year(), date.month(), date.day());
+    this->datiAcquisto->data(xmlData);
+    emit changesOccurred();
+}
+
+void DatiAcquistoWidget::on_prezzo_editingFinished()
+{
+    this->datiAcquisto->prezzo().valore(this->ui->prezzo->value());
+    emit changesOccurred();
 }
