bbbbcaa87061105d044eb0b303a758972f70a682
mirkoviviani76 <mirkoviviani76@gmail.com>
2014-10-09 14:23:35 +0000

    Aggiunto filtro sulle monete

--- GestCollOnQt/GUI/Collezione/collezionesortfilterproxymodel.cpp
+++ GestCollOnQt/GUI/Collezione/collezionesortfilterproxymodel.cpp
@@ -128,3 +128,20 @@ MonetaXml* CollezioneSortFilterProxyModel::getItem(const QModelIndex &index) {
 }
 
 
+
+
+bool CollezioneSortFilterProxyModel::filterAcceptsRow(int sourceRow,
+         const QModelIndex &sourceParent) const
+ {
+     QModelIndex index0 = sourceModel()->index(sourceRow, 0, sourceParent);
+     QModelIndex index1 = sourceModel()->index(sourceRow, 1, sourceParent);
+     QModelIndex index2 = sourceModel()->index(sourceRow, 2, sourceParent);
+     QModelIndex index3 = sourceModel()->index(sourceRow, 3, sourceParent);
+     QModelIndex index4 = sourceModel()->index(sourceRow, 4, sourceParent);
+
+     return (sourceModel()->data(index0).toString().contains(filterRegExp())
+             || sourceModel()->data(index1).toString().contains(filterRegExp())
+             || sourceModel()->data(index2).toString().contains(filterRegExp())
+             || sourceModel()->data(index3).toString().contains(filterRegExp())
+             || sourceModel()->data(index4).toString().contains(filterRegExp()));
+ }
